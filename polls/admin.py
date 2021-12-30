f78e21efa0767837f9a6be545a469ead70b8988a
Hamed_D3 <hameddaneshvar3@gmail.com>
2021-12-30 12:43:17 +0330

    change something necessary things for admin panel

--- polls/admin.py
+++ polls/admin.py
@@ -1,4 +1,21 @@
 from django.contrib import admin
-from .models import Question
+from .models import Choice, Question
 
-admin.site.register(Question)
+
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Question information', {'fields': ['question_text']}),
+        ('Data information',     {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInLine]
+
+    list_display = ['question_text', 'pub_date', 'was_published_recently']
+    list_filter = ['pub_date']
+    search_fields = ['question_text'] 
+
+admin.site.register(Question, QuestionAdmin)
